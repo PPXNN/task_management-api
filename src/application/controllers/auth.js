8d0dcd43ad4f768434318bbe3ea2f3750b4a20c5
Thapanan Suwansukhum <thapanansu555@gmail.com>
2025-07-30 16:34:35 +0700

    add change password and forgot password

--- src/application/controllers/auth.js
+++ src/application/controllers/auth.js
@@ -44,4 +44,60 @@ const login = async (req, res) => {
 
 }
 
-module.exports = { register, login};
+const change_password = async (req, res) => {
+    const {username, old_password, new_password, confirm_new_password} = req.body;
+    const db = await conn();
+
+    const [user_check] = await db.execute("SELECT * FROM users WHERE username = ?", [username]);
+    if (user_check.length === 0){
+        return res.status(400).json({error : "Username is not found"})
+    }
+
+    const user = user_check[0];
+    const compare_password = await bcrypt.compare(old_password, user.password);
+
+    if (!compare_password){
+        return res.status(400).json({ error: "Your old password is incorrect"})
+    }
+
+    if (new_password === confirm_new_password){
+        const hash_password = await bcrypt.hash(new_password, 10);
+        await db.execute("UPDATE users SET password = ? WHERE username = ?", [hash_password, username]);
+
+        return res.status(201).json({message : "Password has changed successfully"})
+    }
+
+    res.status(400).json({error : "new password or confirm new_password does not match"})
+}
+
+const forgot_password = async (req, res) => {
+    const {username, email, phone_number, new_password, confirm_new_password} = req.body;
+    const db = await conn();
+
+    const [user_check] = await db.execute("SELECT * FROM users WHERE username = ?", [username]);
+    if (user_check.length === 0){
+        return res.status(400).json({error : "Username is not found"});
+    }
+
+    const user = user_check[0];
+
+    const [user_info_check] = await db.execute("SELECT * FROM user_info WHERE user_id = ? AND phone_number = ? AND email = ?", [user.user_id, phone_number, email]);
+    if (user_info_check.length === 0){
+        return res.status(400).json({error : "Phone number or email does not match"});
+    }
+
+    if (new_password === confirm_new_password){
+        const hash_password = await bcrypt.hash(new_password, 10);
+
+        await db.execute("UPDATE users SET password = ? WHERE user_id = ?", [hash_password, user.user_id])
+        return res.status(201).json({message : "Password has changed successfully"})
+    }
+
+    res.status(400).json({error : "new password or confirm new_password does not match"})
+
+
+
+
+}
+
+module.exports = { register, login, change_password, forgot_password};
